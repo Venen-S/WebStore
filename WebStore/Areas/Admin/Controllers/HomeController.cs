89a7f96b675ed1cfb51c4aca3d3db4996fccaf6c
Николай <semlya.kolyan@mail.ru>
2020-02-10 20:47:56 +0300

    Занятие 8 админка

---
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Filters;
+using WebStore.Infrastructure.Interface;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly IProductData _productData;
+
+        public HomeController(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            return View(_productData.GetProducts(new ProductFilter()));
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -83,9 +83,12 @@ namespace WebStore
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-               
 
                 endpoints.Map("/hello", async context =>
                 {
