c2fab85a1d24a00694c10b79966e0ada4925ae10
Николай <semlya.kolyan@mail.ru>
2020-05-16 01:28:52 +0300

    Контроллер Web Api

---
+++ Services/WebStore.Interfaces/Api/IValueServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+
+namespace WebStore.Interfaces.Api
+{
+    public interface IValueServices
+    {
+        IEnumerable<string> Get();
+        string Get(int id);
+        Uri Post(string value);
+        HttpStatusCode Update(int id, string value);
+        HttpStatusCode Delete(int id);
+    }
+}
