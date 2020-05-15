606a01b6dd583991b9a0e1a23013ed08fa416e94
Николай <semlya.kolyan@mail.ru>
2020-05-16 02:00:47 +0300

    1 урок 2.1 finally Реализован WebAPI тестовый.

---
+++ Services/WebStore.Clients/Values/ValuesClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Interfaces.Api;
+
+namespace WebStore.Clients.Values
+{
+    public class ValuesClient:BaseClient,IValueServices
+    {
+        public ValuesClient(IConfiguration Configuration):base(Configuration,"api/values")
+        {
+            
+        }
+
+        public IEnumerable<string> Get()
+        {
+            var respons = _Client.GetAsync(_ServiceAddress).Result;
+            if (respons.IsSuccessStatusCode)
+                return respons.Content.ReadAsAsync<IEnumerable<string>>().Result;
+            return Enumerable.Empty<string>();
+        }
+
+        public string Get(int id)
+        {
+            var response = _Client.GetAsync($"{_ServiceAddress}/{id}").Result;
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsAsync<string>().Result;
+            return string.Empty;
+        }
+
+        public Uri Post(string value)
+        {
+            var response = _Client.PostAsJsonAsync($"{_ServiceAddress}/post", value).Result;
+            return response.EnsureSuccessStatusCode().Headers.Location;
+        }
+
+        public HttpStatusCode Update(int id, string value)
+        {
+            var response = _Client.PutAsJsonAsync($"{_ServiceAddress}/put/{id}", value).Result;
+            return response.EnsureSuccessStatusCode().StatusCode;
+        }
+
+        public HttpStatusCode Delete(int id)
+        {
+            var response = _Client.DeleteAsync($"{_ServiceAddress}/delete/{id}").Result;
+            return response.StatusCode;
+        }
+    }
+}
---
+++ UI/WebStore/Controllers/WebApiTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Api;
+
+namespace WebStore.Controllers
+{
+    public class WebApiTestController : Controller
+    {
+        private readonly IValueServices _valueServices;
+        public WebApiTestController(IValueServices ValuesServices) => _valueServices = ValuesServices;
+        
+        public IActionResult Index()
+        {
+            return View(_valueServices.Get());
+        }
+    }
+}
