c37c4d3c401bfaeff8f2eafdeaff882fcbb66da4
Николай <semlya.kolyan@mail.ru>
2020-05-17 17:06:29 +0300

    Заново реализован Web Api с fix багом.

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore.Clients.Values;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces.Api;
 using WebStore.Interfaces.Services;
 using WebStoreServices.Data;
 using WebStoreServices.Products.InCookies;
@@ -69,6 +71,8 @@ namespace WebStore
             services.AddScoped<IProductData, SqlProductData>();
             services.AddScoped<ICartService, CookiesCartService>();
             services.AddScoped<IOrderService, SqlOrderService>();
+
+            services.AddScoped<IValueServices, ValuesClient>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WebStoreDBInitializer db)
