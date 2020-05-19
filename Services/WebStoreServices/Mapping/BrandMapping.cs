0c0c23ed9f3e529ee3a68d67f8c8c84392d3fc92
Николай <semlya.kolyan@mail.ru>
2020-05-19 16:28:35 +0300

    проекции DTO

---
+++ Services/WebStoreServices/Mapping/BrandMapping.cs
@@ -0,0 +1,24 @@
+﻿using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+
+namespace WebStoreServices.Mapping
+{
+    public static class BrandMapping
+    {
+        public static BrandDTO ToDTO(this Brand Brand) => Brand is null
+            ? null
+            : new BrandDTO()
+            {
+                Id = Brand.Id,
+                Name = Brand.Name,
+            };
+
+        public static Brand FromDTO(this BrandDTO Brand) => Brand is null
+            ? null
+            : new Brand()
+            {
+                Id = Brand.Id,
+                Name = Brand.Name,
+            };
+    }
+}
