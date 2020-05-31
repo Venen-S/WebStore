a452240a04fb60d3bd71f50dbc642af63f782a0b
Николай <semlya.kolyan@mail.ru>
2020-05-31 13:23:26 +0300

    Конфигурация serilog в файле

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,5 +1,12 @@
+using System;
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 namespace WebStore
 {
@@ -15,20 +22,29 @@ namespace WebStore
                 .ConfigureLogging((host, log) =>
                 {
                     //log.ClearProviders();
-                    //log.AddProvider(new ConsoleLoggerProvider());
+                    //log.AddProvider(new ConsoleLoggerProvider())
                     //log.AddConsole(opt => opt.IncludeScopes = true);
                     //log.AddEventLog(opt => opt.LogName = "WebStore");
                     //log.AddFilter("System", LogLevel.Warning);
                     //log.AddFilter((category, level) =>
                     //{
-                    //    if (category.StartsWith("Microsoft"))
+                    //    if (category.StartsWith("Microsoft")) 
                     //        return level >= LogLevel.Warning;
                     //    return true;
                     //});
                 })
-                .ConfigureWebHostDefaults(builder =>
-                {
-                    builder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(builder => { builder.UseStartup<Startup>(); })
+                .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(
+                        outputTemplate:
+                        "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                    .WriteTo.RollingFile($@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                    .WriteTo.File(new JsonFormatter(",", true),
+                        $@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json"));
+
+        //.WriteTo.Seq("http://localhost:5001/"));
     }
 }
