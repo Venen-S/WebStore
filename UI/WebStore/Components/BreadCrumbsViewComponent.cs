c9bd4f69b1890d9d13b1ab45e3011cdb7980f1d4
Николай <semlya.kolyan@mail.ru>
2020-06-02 17:56:25 +0300

    Заготовка для визуализации "хлебных крошек"

---
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class BreadCrumbsViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
